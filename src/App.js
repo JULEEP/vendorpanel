acd537fd4d552db199a81d8bc97b5835364969dc
Rishant Rana <kiturana378@gmail.com>
2025-02-19 14:15:55 +0530

    fix layout and app.jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import React from "react";
 import DepartmentList from "./Components/department";  // Correct the path here
+import { Routes,Route } from "react-router-dom";
+// import AdminLayout from "./Layout/AdminLayout.js";
+import Homepage from "./Pages/Homepage";
+// import { Route } from "lucide-react";
+import AdminLayout from "./Layout/AdminLayout.jsx";
+
 
 
 
 function App() {
   return (
-    <div className="App">
-      <DepartmentList /> {/* Render the DepartmentList component */}
-    </div>
+    <AdminLayout>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/department" element={<DepartmentList/>}/>
+      </Routes>
+    </AdminLayout>
   );
 }
 
